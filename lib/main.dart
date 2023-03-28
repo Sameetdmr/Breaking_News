16c78ecec66094c39fb6cd2d09a4f52522cc6873
SAMED DEMIR <SAMED.DEMIR@lcwaikiki.com>
2023-03-28 20:25:57 +0300

    finish commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,37 +1,14 @@
-import 'package:firebase_core/firebase_core.dart';
-import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:get/get.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:newspaper_app/utils/constants/App_Constants.dart';
-import 'firebase_options.dart';
+import 'package:newspaper_app/utils/initialize/application_start.dart';
 import 'ui/splash/SplashPage.dart';
-import 'utils/servicelocator/ServiceLocator.dart';
 
 GlobalKey<NavigatorState> mainNavigatorKey = GlobalKey();
 
 void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
-
-  FlutterError.onError = (FlutterErrorDetails details) async {
-    try {
-      FirebaseCrashlytics.instance.setCustomKey('Error', details.exception.toString());
-      FirebaseCrashlytics.instance.setCustomKey('Details', details.toString());
-      FirebaseCrashlytics.instance.setCustomKey('StackTrace', StackTrace.current.toString());
-      FirebaseCrashlytics.instance.crash();
-    } catch (ex) {
-      FirebaseCrashlytics.instance.log(details.exception.toString() + ' / ' + details.toString() + " - " + StackTrace.current.toString());
-      FirebaseCrashlytics.instance.crash();
-    }
-  };
-
-  ServiceLocator().init();
-  SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(statusBarColor: Colors.black));
+  await ApplicationStart.init();
   runApp(MyApp());
 }
 
---
+++ lib/utils/initialize/application_start.dart
@@ -0,0 +1,34 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+import 'package:kartal/kartal.dart';
+
+import '../../firebase_options.dart';
+import '../servicelocator/ServiceLocator.dart';
+
+@immutable
+class ApplicationStart {
+  const ApplicationStart._();
+  static Future<void> init() async {
+    WidgetsFlutterBinding.ensureInitialized();
+    await DeviceUtility.deviceInit();
+    await Firebase.initializeApp(
+      options: DefaultFirebaseOptions.currentPlatform,
+    );
+    FlutterError.onError = (FlutterErrorDetails details) async {
+      try {
+        FirebaseCrashlytics.instance.setCustomKey('Error', details.exception.toString());
+        FirebaseCrashlytics.instance.setCustomKey('Details', details.toString());
+        FirebaseCrashlytics.instance.setCustomKey('StackTrace', StackTrace.current.toString());
+        FirebaseCrashlytics.instance.crash();
+      } catch (ex) {
+        FirebaseCrashlytics.instance.log(details.exception.toString() + ' / ' + details.toString() + " - " + StackTrace.current.toString());
+        FirebaseCrashlytics.instance.crash();
+      }
+    };
+    SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(statusBarColor: Colors.black));
+    ServiceLocator().init();
+  }
+}
